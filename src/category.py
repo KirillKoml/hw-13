8e1ece54c67897d5658def6472688b5b6eeb3c18
KirillKoml <144651672+KirillKoml@users.noreply.github.com>
2024-03-29 10:51:30 +0500

    Update category.py

--- src/category.py
+++ src/category.py
@@ -1,6 +1,6 @@
 class Category:
     all_quantity_category = 0
-    all_quantity_unique_product = 0
+    all_quantity_unique_product = set()
     name: str
     description: str
     product: list
@@ -10,22 +10,22 @@ class Category:
         self.description = description
         self.__product = product
         self.all_quantity_category = 1
-
-        Category.all_quantity_unique_product += len(self.__product)
         Category.all_quantity_category += 1
 
-    def product(self):
-        return self.__product
+    def __str__(self):
+        return f'{self.name}, количество продуктов:{len(self)} шт'
+        
+    def product(self,products):
+        self.__product.append(products)
 
     def __len__(self):
         return len(self.__product)
 
-    def __str__(self):
-        return f'{self.name}, количество продуктов:{len(self)} шт'
+
 
     @property
     def updated_product(self):
-        updated_product = ''
+        updated_product = []
         for product in self.__product:
             updated_product.append(f'{product.name},{product.price} руб. Остаток: {product.quantity} шт.')
         return updated_product
